a5c54d27b6c2ef22050b07336529deab486bc567
Stéphane D <derste@gmail.com>
2015-08-31 14:30:57 +0200

    Indent with spaces

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const LANGS = argv.langs.split(',')
 
 const FOLDER = argv._[0];
 if (!FOLDER) {
-	console.error('Please specify a folder');
-	process.exit(0);
+  console.error('Please specify a folder');
+  process.exit(0);
 }
 
 var GLOB = path.join(FOLDER, '**', '*.resx');
@@ -24,93 +24,93 @@ var options = {};
 
 glob(GLOB, options, (err, files) => {
 
-	// create tasks to read all file contents in parallel
-	var tasks = files.filter(filterLang).map((fileName) => {
-		const group = path.basename(fileName).split('.')[0];
-		const lang = getLang(fileName);
+  // create tasks to read all file contents in parallel
+  var tasks = files.filter(filterLang).map((fileName) => {
+    const group = path.basename(fileName).split('.')[0];
+    const lang = getLang(fileName);
 
-		return callback => 
+    return callback => 
 
-			// read each file async
-			fs.readFile(fileName, (err, data) => {
-				if (err) {
-					callback(fileName + ": " + err, null);
-					return;
-				}
+      // read each file async
+      fs.readFile(fileName, (err, data) => {
+        if (err) {
+          callback(fileName + ": " + err, null);
+          return;
+        }
 
-				// when we have the content, parse it
-    		parseXml(data, (err, result) => {
-    			if (err) {
-    				callback(fileName + " xml: " + err, null);
-    				return;
-    			}
+        // when we have the content, parse it
+        parseXml(data, (err, result) => {
+          if (err) {
+            callback(fileName + " xml: " + err, null);
+            return;
+          }
 
-    			if (!result.root.data) {
-    				callback(null, { group, fileName, lang, keys: [] });
-    				return;
-    			}
+          if (!result.root.data) {
+            callback(null, { group, fileName, lang, keys: [] });
+            return;
+          }
 
-    			// and read all its keys
-    			var keys = result.root.data.map(node => node.$.name);
+          // and read all its keys
+          var keys = result.root.data.map(node => node.$.name);
 
-    			// and callback with the results
-    			callback(null, {
-    				group,
-    				fileName,
-    				lang,
-    				keys
-    			});
+          // and callback with the results
+          callback(null, {
+            group,
+            fileName,
+            lang,
+            keys
+          });
 
-    		});
-			});
+        });
+      });
 
-	});
+  });
 
-	// parallel read!
-	async.parallelLimit(tasks, 10, (err, results) => {
-		if (err) {
-			throw Error(err)
-		}
+  // parallel read!
+  async.parallelLimit(tasks, 10, (err, results) => {
+    if (err) {
+      throw Error(err)
+    }
 
-		// groups = [ 'DashboardSettings', 'WidgetSettings', ... ]
-		// we are going to compare the keys of the same group
-		var groups = groupBy(results, (res) => res.group);
+    // groups = [ 'DashboardSettings', 'WidgetSettings', ... ]
+    // we are going to compare the keys of the same group
+    var groups = groupBy(results, (res) => res.group);
 
-		// for each of those resource group
-		Object.keys(groups).forEach((group) => {
+    // for each of those resource group
+    Object.keys(groups).forEach((group) => {
 
-			// find the base lang to get the keys that should exist everywhere
-			const baseLangItem = groups[group].filter(g => g.lang === BASE_LANG)[0];
-			if (!baseLangItem) {
-				console.error('no ' + BASE_LANG + ' found for ' + group);
-				return;
-			}
+      // find the base lang to get the keys that should exist everywhere
+      const baseLangItem = groups[group].filter(g => g.lang === BASE_LANG)[0];
+      if (!baseLangItem) {
+        console.error('no ' + BASE_LANG + ' found for ' + group);
+        return;
+      }
 
-			const baseKeys = baseLangItem.keys;
+      const baseKeys = baseLangItem.keys;
 
-			// check that each language in the group has all those keys
-			groups[group]
-				.filter(notBaseLang)
-				.forEach(result => {
-					const delta = difference(baseKeys, result.keys);
-					if (delta.length > 0) {
-						console.log(`${result.fileName} does not have those keys:`);
-						console.log(delta.join('\n'));
-						console.log();
-					}					
-				});
+      // check that each language in the group has all those keys
+      groups[group]
+        .filter(notBaseLang)
+        .forEach(result => {
+          const delta = difference(baseKeys, result.keys);
+          if (delta.length > 0) {
+            console.log(`${result.fileName} does not have those keys:`);
+            console.log(delta.join('\n'));
+            console.log();
+          }         
+        });
 
-			/**
-			* Return true if the given group is not about the base lang.
-			*/
-			function notBaseLang(group) {
-				return group.lang !== BASE_LANG;
-			}
+      /**
+      * Return true if the given group is not about the base lang.
+      */
+      function notBaseLang(group) {
+        return group.lang !== BASE_LANG;
+      }
 
-		});
+    });
 
 
-	})
+  })
 
 });
 
@@ -119,19 +119,19 @@ glob(GLOB, options, (err, files) => {
  * Return the lang a given resx is about.
  */
 function getLang(fileName) {
-	const lang = path.extname(path.basename(fileName).replace('.resx', '')).split('.')[1];
+  const lang = path.extname(path.basename(fileName).replace('.resx', '')).split('.')[1];
 
-	if (lang === 'aspx' /* localResources: FormulaQuery.aspx.resx */
-	 || lang === undefined  /* globalResources: TeamUserProperties.resx */ ) {
-		return BASE_LANG;
-	}
-	return lang;
+  if (lang === 'aspx' /* localResources: FormulaQuery.aspx.resx */
+   || lang === undefined  /* globalResources: TeamUserProperties.resx */ ) {
+    return BASE_LANG;
+  }
+  return lang;
 }
 
 /**
  * Return true if the given fileName is the base lang or one of the lang we want.
  */
 function filterLang(fileName) {
-	var l = getLang(fileName);
-	return l === BASE_LANG || LANGS.indexOf(getLang(fileName)) >= 0;
+  var l = getLang(fileName);
+  return l === BASE_LANG || LANGS.indexOf(getLang(fileName)) >= 0;
 }
